e5597879fe34b4a00c1b43042502fb582faba9e0
eahmhod <hoda.ahmed@ericsson.com>
2018-08-08 14:35:38 +0100

    more imports

--- app/src/main/java/com/example/eahmhod/myfirstapp2/MainActivity.java
+++ app/src/main/java/com/example/eahmhod/myfirstapp2/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.eahmhod.myfirstapp2;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
